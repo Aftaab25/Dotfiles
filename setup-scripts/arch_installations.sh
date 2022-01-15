b83d0fe744186157b532e2a34cc594f963702049
Aftaab Siddiqui <aftaab2507@gmail.com>
2022-01-15 22:13:46 +0530

    added commands for config files

--- setup-scripts/arch_installations.sh
+++ setup-scripts/arch_installations.sh
@@ -2,93 +2,86 @@
 
 # Installing essential apps
 #sudo pacman -S yay
-echo "Installing the following apps: \n1. Vim\n2. Neovim\n3. xsel(Clipboard for vim & neovim)\n4. nodejs\n5. npm\n6. yarn\n7. zsh\n8. tree\n9. tmux\n"
-echo "===============================================\n"
+echo -e "===============================================\n"
+echo -e "Installing the following apps: \n1. Vim\n2. Neovim\n3. xsel(Clipboard for vim & neovim)\n4. nodejs\n5. npm\n6. yarn\n7. zsh\n8. tree\n9. tmux\n"
+echo -e "===============================================\n"
 sudo pacman -S vim neovim xsel nodejs npm yarn zsh tree tmux
 
 # Installing Courier-Code font
+echo -e "===============================================\n"
+echo -e "Installing Courier-Code font\n"
+echo -e "===============================================\n"
 yay -S ttf-courier-code
 
 # Installing jdk-17 & jre-17
-echo "Installing JAVA (jdk-17 & jre-17)\n"
-echo "===============================================\n"
+echo -e "===============================================\n"
+echo -e "Installing JAVA (jdk-17 & jre-17)\n"
+echo -e "===============================================\n"
 yay -S jdk jre
 
 # For installing browsers, IDEs, text-editors and other apps
-echo "Installing Browsers: Google Chrome, Epiphany, Brave\n"
-echo "===============================================\n"
+echo -e "===============================================\n"
+echo -e "Installing Browsers: Google Chrome, Epiphany, Brave\n"
+echo -e "===============================================\n"
 yay -S brave-bin google-chrome epiphany brave-bin
 
-echo "Installing IDEs and text-editors\n"
-echo "===============================================\n"
+echo -e "===============================================\n"
+echo -e "Installing IDEs and text-editors\n"
+echo -e "===============================================\n"
 yay -S intellij-idea-community-edition pycharm-community-edition visual-studio-code-insiders-bin visual-studio-code-bin geany
 yay -S sublime-text-4 notepadqq 
 
-echo "Installing Dev Tools\n"
-echo "===============================================\n"
+echo -e "===============================================\n"
+echo -e "Installing Dev Tools\n"
+echo -e "===============================================\n"
 yay -S github-desktop-bin gimp krita
 
-echo "Installing IDEs and text-editors: Intellij-Idea\n"
-echo "===============================================\n"
-yay -S spotify telegram-desktop-bin vlc okular libreoffice-still
+echo -e "===============================================\n"
+echo -e "Installing other apps\n"
+echo -e "===============================================\n"
+yay -S spotify telegram-desktop-bin vlc okular libreoffice-still whatsapp-for-linux discord slack
 #yay -S alacritty
 
 #installing oh-my-zsh
-echo "Installing ohmyzsh\n"
-echo "===============================================\n"
+echo -e "===============================================\n"
+echo -e "Installing ohmyzsh\n"
+echo -e "===============================================\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-# Installing brave browser
-
-# Creating necessary directories
-# alacritty dir
-cd .config
-mkdir alacritty
-cd alacritty
-cd
-
-# nvim dir
-cd .config
-mkdir nvim
-cd
-
-# kitty dir
-cd .config
-mkdir kitty
-cd kitty
-
-
-# clone git repos for configuration files
-# cloning alacritty config
-git clone https://github.com/Aftaab25/Alacritty-Config.git
-cd Alacritty-Config
-mv alacritty.yml ~/.config/alacritty/
-sudo mv jetbrains-mono-nerd-patched.ttf /usr/share/fonts/
-cd
-
-
-# cloning kitty config
-git clone https://github.com/Aftaab25/kitty-config.git
-cd kitty-config
-sudo mv JetBrains* /usr/share/fonts/
-sudo mv FiraCode/ /usr/share/fonts/
-mv kitty.conf ~/.config/kitty/
-cd
-
-# cloning nvim config
+# Installing vim-plug for vim and neovim
+echo -e "===============================================\n"
+echo -e "Installing vim-plug for vim\n"
+echo -e "===============================================\n"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo -e "===============================================\n"
+echo -e "Installing vim-plug for neovim\n"
+echo -e "===============================================\n"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-git clone https://github.com/Aftaab25/nvim-config-new.git
-cd nvim-config-new
-mv init.vim ~/.config/nvim/
-cd
+# Moving .vimrc to ~/.config folder
+echo -e "===============================================\n"
+echo -e "Moving(Copying) .vimrc to ~/.config folder\n"
+echo -e "===============================================\n"
+cp ~/Dotfiles/.vimrc ~/.
 
-# cloning .vimrc
-git clone https://github.com/Aftaab25/vim-vimrc.git
-cd vim-vimrc
-mv .vimrc ~/
+# Moving nvim-config-new to ~/.config/nvim
+echo -e "===============================================\n"
+echo -e "Moving(Copying) nvim-config-new to ~/.config/nvim\n"
+echo -e "===============================================\n"
+cp ~/Dotfiles/nvim-config-new ~/.config/nvim
+
+# Moving Jetbrains Font from Kitty folder to /usr/share/fonts/
+echo -e "===============================================\n"
+echo -e "Moving Jetbrains Font from Dotfiles/kitty/ folder to /usr/share/fonts/\n"
+echo -e "sudo permissions will be reqd."
+echo -e "===============================================\n"
+sudo mv ~/Dotfiles/kitty/Jet* /usr/share/fonts/
+
+echo -e "===============================================\n"
+echo -e "                      DONE                       "
+echo -e "===============================================\n"
