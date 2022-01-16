cc5bd678ea21d5dc8d3567c957f11307b12e4ffc
Aftaab Siddiqui <aftaab2507@gmail.com>
2022-01-17 00:04:34 +0530

    Added more apps and new links
    Added more apps and new links. Removed ohmyzsh.

--- setup-scripts/debian_installations.sh
+++ setup-scripts/debian_installations.sh
@@ -6,13 +6,15 @@ echo -e "Installing the following apps: \n1. Vim\n2. Neovim\n3. xsel(Clipboard f
 echo -e "===============================================\n"
 sudo apt install vim neovim xsel nodejs gnome-tweaks curl wget npm yarn zsh tree tmux ubuntu-restricted-extras
 
+
 #installing oh-my-zsh
-echo -e "===============================================\n"
-echo -e "Installing ohmyzsh\n"
-echo -e "===============================================\n"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#echo -e "===============================================\n"
+#echo -e "Installing ohmyzsh\n"
+#echo -e "===============================================\n"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
 
 # Download Spotify for Linux
 echo -e "===============================================\n"
@@ -24,58 +26,49 @@ sudo apt-get update && sudo apt-get install spotify-client
 
 
 # installing brave browser
+echo -e "===============================================\n"
+echo -e "Installing Brave Browser\n"
+echo -e "===============================================\n"
 sudo apt install apt-transport-https
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update && sudo apt install brave-browser
-
-# Creating necessary directories
-# alacritty dir
-cd .config
-mkdir alacritty
-cd alacritty
-cd
-
-# nvim dir
-cd .config
-mkdir nvim
-cd
-
-# kitty dir
-cd .config
-mkdir kitty
-cd kitty
+sudo apt update
+sudo apt install brave-browser
 
 
-# clone git repos for configuration files
-# cloning alacritty config
-git clone https://github.com/Aftaab25/Alacritty-Config.git
-cd Alacritty-Config
-mv alacritty.yml ~/.config/alacritty/
-sudo mv jetbrains-mono-nerd-patched.ttf /usr/share/fonts/
-cd
+# Installing jdk-17 & jre-17
+echo -e "===============================================\n"
+echo -e "Installing JAVA (jdk-17 & jre-17)\n"
+echo -e "===============================================\n"
+sudo apt install openjdk-17-jre-headless openjdk-17-jre-headless
 
 
-# cloning kitty config
-git clone https://github.com/Aftaab25/kitty-config.git
-cd kitty-config
-sudo mv JetBrains* /usr/share/fonts/
-sudo mv FiraCode/ /usr/share/fonts/
-mv kitty.conf ~/.config/kitty/
-cd
+# installing Epiphany browser
+echo -e "===============================================\n"
+echo -e "Installing Epiphany Browser\n"
+echo -e "===============================================\n"
+sudo apt install epiphany-browser
 
-# cloning nvim config
+
+# installing Notepadqq
+echo -e "===============================================\n"
+echo -e "Installing Notepadqq\n"
+echo -e "===============================================\n"
+sudo apt install notepadqq gimp krita obs-studio vlc 
+
+
+# Installing vim-plug for vim and neovim
+echo -e "===============================================\n"
+echo -e "Installing vim-plug for vim\n"
+echo -e "===============================================\n"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo -e "===============================================\n"
+echo -e "Installing vim-plug for neovim\n"
+echo -e "===============================================\n"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-git clone https://github.com/Aftaab25/nvim-config-new.git
-cd nvim-config-new
-mv init.vim ~/.config/nvim/
-cd
 
-# cloning .vimrc
-git clone https://github.com/Aftaab25/vim-vimrc.git
-cd vim-vimrc
-mv .vimrc ~/
+
