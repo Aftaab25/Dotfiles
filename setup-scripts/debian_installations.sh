8de30815cc303cb3f15d724a4759c246ecee04c8
Aftaab Siddiqui <aftaab2507@gmail.com>
2022-01-16 23:52:05 +0530

    updated links for ohmyzsh and spotify
    updated links for ohmyzsh and spotify.

--- setup-scripts/debian_installations.sh
+++ setup-scripts/debian_installations.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 # Installing essential apps
-sudo apt install preload vim neovim gnome-tweaks xsel nodejs npm yarn zsh nitrogen arandr kitty curl git wget tree tmux ubuntu-restricted-extras
+echo -e "===============================================\n"
+echo -e "Installing the following apps: \n1. Vim\n2. Neovim\n3. xsel(Clipboard for vim & neovim)\n4. nodejs\n5. npm\n6. yarn\n7. zsh\n8. tree\n9. tmux\n"
+echo -e "===============================================\n"
+sudo apt install vim neovim xsel nodejs gnome-tweaks curl wget npm yarn zsh tree tmux ubuntu-restricted-extras
 
 #installing oh-my-zsh
+echo -e "===============================================\n"
+echo -e "Installing ohmyzsh\n"
+echo -e "===============================================\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Download Spotify for Linux
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo -e "===============================================\n"
+echo -e "Installing Spotify for Linux\n"
+echo -e "===============================================\n"
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
+
 
 # installing brave browser
 sudo apt install apt-transport-https
