55f1bfecba115505a458c12d119d07f56f8d8af6
Aftaab Siddiqui <aftaab2507@gmail.com>
2022-08-16 19:05:32 +0530

    Deleted commands for File movements

--- setup-files/arch-based/arch_installations.sh
+++ setup-files/arch-based/arch_installations.sh
@@ -63,25 +63,6 @@ echo -e "===============================================\n"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Moving .vimrc to ~/.config folder
-echo -e "===============================================\n"
-echo -e "Moving(Copying) .vimrc to ~/.config folder\n"
-echo -e "===============================================\n"
-cp ~/Dotfiles/.vimrc ~/.
-
-# Moving nvim-config-new to ~/.config/nvim
-echo -e "===============================================\n"
-echo -e "Moving(Copying) nvim-config-new to ~/.config/nvim\n"
-echo -e "===============================================\n"
-cp ~/Dotfiles/nvim-config-new ~/.config/nvim
-
-# Moving Jetbrains Font from Kitty folder to /usr/share/fonts/
-echo -e "===============================================\n"
-echo -e "Moving Jetbrains Font from Dotfiles/kitty/ folder to /usr/share/fonts/\n"
-echo -e "sudo permissions will be reqd."
-echo -e "===============================================\n"
-sudo mv ~/Dotfiles/kitty/Jet* /usr/share/fonts/
-
 echo -e "===============================================\n"
 echo -e "                      DONE                       "
 echo -e "===============================================\n"
