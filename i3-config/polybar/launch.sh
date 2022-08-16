18e9ca8a740c6a2168f71c8236b9442c20d41c25
Aftaab Siddiqui <aftaab2507@gmail.com>
2022-08-16 18:42:40 +0530

    Added Kernel Virtualization Arch

--- i3-config/polybar/launch.sh
+++ i3-config/polybar/launch.sh
@@ -8,6 +8,6 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar example1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar example2 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar example2 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
