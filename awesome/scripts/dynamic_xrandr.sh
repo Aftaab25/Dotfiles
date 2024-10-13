631c980aa00f5072efe00902dd90e023e79ebb21
aftaab25 <aftaab2507@gmail.com>
2024-10-14 01:22:41 +0530

    New refresh rate for screens

--- awesome/scripts/dynamic_xrandr.sh
+++ awesome/scripts/dynamic_xrandr.sh
@@ -3,11 +3,11 @@
 # Check if HDMI-1-0 is connected
 if xrandr | grep "HDMI-1-0 connected"; then
     # If HDMI-1-0 is connected, configure both eDP-1 and HDMI-1-0
-    xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-           --output HDMI-1-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+    xrandr --output eDP-1 --mode 1920x1080 --rate 120 --pos 0x0 --rotate normal \
+           --output HDMI-1-0 --primary --mode 1920x1080 --rate 75 --pos 1920x0 --rotate normal
 else
     # If HDMI-1-0 is not connected, only configure eDP-1
-    xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+    xrandr --output eDP-1 --mode 1920x1080 --rate 120 --pos 0x0 --rotate normal \
            --output HDMI-1-0 --off
 fi
 
